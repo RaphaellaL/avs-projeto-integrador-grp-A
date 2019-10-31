00b98d535418807045fdf708169003ed708f3dce
RaphaellaL <raphaella_lopes@yahoo.com.br>
2019-10-31 18:31:40 -0300

    .

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
